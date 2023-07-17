2965cdd2d9323a9901558e383b7df14c868be178
Sema Ç <semacelikten22@gmail.com>
2023-07-17 22:21:31 +0300

    PageHomeController oluşturuldu

---
+++ app/Http/Controllers/PageHomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageHomeController extends Controller
+{
+    public function anasayfa(){
+        return view('welcome');
+    }
+}
